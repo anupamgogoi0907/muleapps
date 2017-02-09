4652e9a9a7d9697e619bb1ec510b1f21d2d89f4f
Anupam Gogoi <ac-agogoi@192.168.100.87>
2017-02-09 16:53:12 -0200

    code changed.

--- object-datastore-example/src/main/java/com/anupam/os/MyObjectStore.java
+++ object-datastore-example/src/main/java/com/anupam/os/MyObjectStore.java
@@ -1,12 +1,38 @@
 package com.anupam.os;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.mule.api.MuleContext;
 import org.mule.api.store.ObjectStore;
 import org.mule.api.store.ObjectStoreException;
-import org.mule.util.store.AbstractObjectStore;
+import org.mule.transport.jdbc.JdbcConnector;
 
 public class MyObjectStore implements ObjectStore<Serializable> {
+	private DataSource dataSource;
+	private String insertQueryKey;
+	private String selectQueryKey;
+	private String deleteQueryKey;
+	private String clearQueryKey;
+
+	@Inject
+	MuleContext muleContext;
+
+	JdbcConnector jdbcConnector;
+
+	public void init() {
+		System.out.println("initialised");
+		jdbcConnector=new JdbcConnector(muleContext);
+		jdbcConnector.setDataSource(dataSource);
+		Map queries=new HashMap();
+		queries.put("insertQueryKey", insertQueryKey);
+		jdbcConnector.setQueries(queries);
+		
+	}
 
 	@Override
 	public boolean contains(Serializable key) throws ObjectStoreException {
@@ -16,8 +42,8 @@ public class MyObjectStore implements ObjectStore<Serializable> {
 
 	@Override
 	public void store(Serializable key, Serializable value) throws ObjectStoreException {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Storing Data");
+
 	}
 
 	@Override
@@ -41,9 +67,47 @@ public class MyObjectStore implements ObjectStore<Serializable> {
 	@Override
 	public void clear() throws ObjectStoreException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public String getInsertQueryKey() {
+		return insertQueryKey;
+	}
+
+	public void setInsertQueryKey(String insertQueryKey) {
+		this.insertQueryKey = insertQueryKey;
+	}
+
+	public String getSelectQueryKey() {
+		return selectQueryKey;
+	}
+
+	public void setSelectQueryKey(String selectQueryKey) {
+		this.selectQueryKey = selectQueryKey;
+	}
+
+	public String getDeleteQueryKey() {
+		return deleteQueryKey;
+	}
+
+	public void setDeleteQueryKey(String deleteQueryKey) {
+		this.deleteQueryKey = deleteQueryKey;
+	}
+
+	public String getClearQueryKey() {
+		return clearQueryKey;
+	}
+
+	public void setClearQueryKey(String clearQueryKey) {
+		this.clearQueryKey = clearQueryKey;
+	}
 
 }
