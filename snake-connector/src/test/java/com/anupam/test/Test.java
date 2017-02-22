f390cefc2d9a031efdbfac166c84bcd43062479c
Anupam Gogoi <ac-agogoi@192.168.100.87>
2017-02-22 19:04:57 -0300

    test

--- snake-connector/src/test/java/com/anupam/test/Test.java
+++ snake-connector/src/test/java/com/anupam/test/Test.java
@@ -1,6 +1,7 @@
 package com.anupam.test;
 
-import com.anupam.snake.connector.SnakeConnector;
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.mule.common.Result;
@@ -8,45 +9,40 @@ import org.mule.common.metadata.MetaData;
 import org.mule.common.metadata.MetaDataKey;
 import org.mule.tools.devkit.ctf.mockup.ConnectorDispatcher;
 import org.mule.tools.devkit.ctf.mockup.ConnectorTestContext;
-import org.mule.util.scan.annotations.Meta;
 
-import java.util.List;
+import com.anupam.snake.connector.SnakeConnector;
 
 /**
  * Created by ac-agogoi on 2/21/17.
  */
 public class Test {
 
-    @BeforeClass
-    public static void setUp()
-    {
-        ConnectorTestContext.initialize(SnakeConnector.class);
-    }
+	@BeforeClass
+	public static void setUp() {
+		ConnectorTestContext.initialize(SnakeConnector.class);
+	}
 
-    @org.junit.Test
-    public void test()
-    {
+	@org.junit.Test
+	public void test() {
 
-        ConnectorDispatcher<SnakeConnector> dispatcher=getDispatcher();
-        Result<List<MetaDataKey>> keys=dispatcher.fetchMetaDataKeys();
-        List<MetaDataKey> listKeys=keys.get();
+		ConnectorDispatcher<SnakeConnector> dispatcher = getDispatcher();
+		Result<List<MetaDataKey>> keys = dispatcher.fetchMetaDataKeys();
+		List<MetaDataKey> listKeys = keys.get();
 
-        MetaDataKey key_posts=listKeys.get(3);
-        Result<MetaData> metaDataResult=dispatcher.fetchMetaData(key_posts);
-        System.out.print("");
-    }
+		MetaDataKey key_posts = listKeys.get(2);
+		Result<MetaData> metaDataResult = dispatcher.fetchMetaData(key_posts);
+		System.out.print("");
+	}
 
-    public ConnectorDispatcher<SnakeConnector> getDispatcher()
-    {
-        ConnectorTestContext<SnakeConnector> context=ConnectorTestContext.getInstance();
-        ConnectorDispatcher<SnakeConnector> dispatcher=context.getConnectorDispatcher();
-        return dispatcher;
-    }
+	public ConnectorDispatcher<SnakeConnector> getDispatcher() {
+		ConnectorTestContext<SnakeConnector> context = ConnectorTestContext.getInstance();
+		ConnectorDispatcher<SnakeConnector> dispatcher = context.getConnectorDispatcher();
+		return dispatcher;
+	}
 
+	@AfterClass
+	public static void shutdownSuite() throws Exception {
 
-    @AfterClass
-    public static void shutdownSuite() throws Exception{
-
-        ConnectorTestContext.shutDown();
-    }
+		ConnectorTestContext.shutDown();
+	}
 }
